e55bc8df6c2d2b86e6c15ec74f9a9afa7163ae6b
aldcejam <aldcejamj923@gmail.com>
2023-12-12 08:06:18 -0300

    BACKEND: add error exception in useCase FindAffiliateByRetailer

--- backend/src/useCases/affiliate/findAffiliateByRetailer/FindAffiliateByRetailerUseCase.ts
+++ backend/src/useCases/affiliate/findAffiliateByRetailer/FindAffiliateByRetailerUseCase.ts
@@ -1,13 +1,19 @@
 import { AffiliateRepository } from "@domain/affiliate/AffiliateRepository";
 import { FindAffiliateByRetailerDTO } from "@dtos/affiliate/FindAffiliateByRetailerDTO";
+import { HttpException } from "@nestjs/common";
 import { Affiliate } from "@prisma/client";
 
 class FindAffiliateByRetailerUseCase{
     constructor(private affiliateRepository: AffiliateRepository){}
 
     async execute({retailerId}: FindAffiliateByRetailerDTO): Promise<Affiliate[]>{
-        const affiliate = await this.affiliateRepository.findByRetailerId(retailerId);
-        return affiliate;
+
+        try{
+            const affiliate = await this.affiliateRepository.findByRetailerId(retailerId);
+            return affiliate;
+        }catch(err){
+            throw new HttpException("Afiliado não encontrado", 404);
+        } 
     }
 }
 
